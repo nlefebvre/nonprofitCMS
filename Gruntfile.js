2e96c1109d18e30b1ef3e5b1c597f6c32c1696fe
Lefebvre, Nicolas <Nicolas_Lefebvre@intuit.com>
2015-04-14 13:33:41 -0700

    added menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,8 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask("default", "Start web server", function() {
+
+  grunt.registerTask("webServer", function(port) {
 
     var
       webServer = require("./web-server"),
@@ -15,9 +16,15 @@ module.exports = function(grunt) {
 
     this.async();
 
+    webServerConfig.port = port || webServerConfig.port;
+    console.log(webServerConfig.port);
+
     webServer(webServerConfig,  function() {
       grunt.log.writeln("Web server listening on port " + webServerConfig.port);
     });
 
   });
+
+  grunt.registerTask("default", "Start web server", ["webServer"]);
+
 };
